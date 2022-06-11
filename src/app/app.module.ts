4ce6dc0bada4201721a883908662a2b800842331
VincentGichohi <vincentegichohi@gmail.com>
2022-06-11 12:11:13 +0300

    modify the module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './server/server.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  //registering the component from server.component.ts
+    ServerComponent
   ],
   imports: [
     BrowserModule
